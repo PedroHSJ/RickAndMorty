b14312f36c69e2262fb8cd6b394896eeaedd9f6a
Pedro Henrique <89044375+PedroHSJ@users.noreply.github.com>
2021-12-11 21:26:04 -0300

    Add files via upload

--- ScriptPers.js
+++ ScriptPers.js
@@ -5,16 +5,15 @@ function LoadPers(NomeDoPersonagem) {
     axios.get(`https://rickandmortyapi.com/api/character/?name=${NomeDoPersonagem}`)
         .then(resp => {
             const linhas = resp.data.results.map(personagem => {
-                
-                return `<div><h2>${personagem.name}</h2><br><img src="${personagem.image}"></img><br><p>${personagem.species}</p></div>`
-                
-              
 
-            })
+                return `<div><h2>${personagem.name}</h2><br><img src="${personagem.image}"></img><br><p>${personagem.species}</p></div>`
+
+
+            }).join('');
             console.log(resp.data.results)
-            conteudopers.innerHTML = linhas ;
+            conteudopers.innerHTML = linhas;
         })
-        
+
 }
 
 
@@ -26,28 +25,38 @@ function enviar() {
     LoadPers(NomeDoPersonagem)
 }
 
-window.addEventListener('keydown', function (e){
-    if (e.code === 'Enter' || e.code === 'NumpadEnter'){
+window.addEventListener('keydown', function (e) {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
         enviar()
     }
 });
 
 
-    axios.get(`https://rickandmortyapi.com/api/character/`)
-        .then(resp => {
-            const linhas = resp.data.results.map(personagem => {
-                
-                return `<div><h2>${personagem.name}</h2><br><img src="${personagem.image}"></img><br><p>${personagem.species}</p></div>`
-                
-              
+axios.get(`https://rickandmortyapi.com/api/character/`)
+    .then(resp => {
+        const linhas = resp.data.results.map(personagem => {
 
-            })
-            console.log(resp.data.results)
-            conteudopers.innerHTML = linhas ;
-        })
+            return `<div><h2>${personagem.name}</h2><br><img src="${personagem.image}"></img><br><p>${personagem.species}</p></div>`
 
 
 
+        }).join('');
+        console.log(resp.data.results)
+        conteudopers.innerHTML = linhas;
+    })
+
+const theme = window.localStorage.getItem("theme");
+const toggle = document.getElementById('toggle');
+
+if (theme === 'dark') document.body.classList.add("dark");
+
+toggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+
+    if (theme === 'dark') {
+        window.localStorage.setItem('light');
+    } else window.localStorage.setItem('dark')
+})
 
 
 
